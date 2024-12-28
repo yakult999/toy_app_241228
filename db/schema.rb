b3329d283af9330a1943a19678fff7dbe51b0e0d
yakult999 <healthy2429@gmail.com>
2024-12-28 13:20:01 +0000

    create microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_28_122539) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_28_130734) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
